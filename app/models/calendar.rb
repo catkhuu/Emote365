b3d8659f2e129d558efe165d1a2f8804de96eb33
levytsroman <r.levyts@gmail.com>
2017-01-04 15:44:02 -0500

    add navbar and footer

--- app/models/calendar.rb
+++ app/models/calendar.rb
@@ -1,2 +1,3 @@
 class Calendar < ApplicationRecord
+  belongs_to :user
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
   get 'welcome/index'
   root 'welcome#index'
+  get 'users/logout'
 
   resources :calendars
 end
