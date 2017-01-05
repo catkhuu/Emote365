4583ed12d331c4b5ca5b445312d2c72c482f4b8a
levytsroman <r.levyts@gmail.com>
2017-01-04 20:39:22 -0500

    add microsoft api call

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -40,7 +40,31 @@ class CalendarsController < ApplicationController
   end
 
   def get_emotions(text)
-    
+    uri = URI('https://westus.api.cognitive.microsoft.com/text/analytics/v2.0/sentiment')
+
+    request = Net::HTTP::Post.new(uri.request_uri)
+    # Request headers
+    request['Content-Type'] = 'application/json'
+    # Request headers
+    request['Ocp-Apim-Subscription-Key'] = ENV[MICROSOFT_KEY]
+    # Request body
+    request.body = {
+      "documents": [
+        {
+          "language": "en",
+          "id": "string",
+          "text": text
+        }
+      ]
+    }.to_json
+
+    response = Net::HTTP.start(uri.host, uri.port, :use_ssl => uri.scheme == 'https') do |http|
+        http.request(request)
+    end
+
+
+
+    JSON.parse(response.body)["documents"][0]["score"]
   end
 
   def get_image()
