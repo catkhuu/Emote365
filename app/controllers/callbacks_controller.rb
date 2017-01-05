5037275d11534c59a52d2bb9138c0e439c92f3b7
levytsroman <r.levyts@gmail.com>
2017-01-04 19:43:37 -0500

    add update_email form

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -1,7 +1,8 @@
 class CallbacksController < Devise::OmniauthCallbacksController
     def twitter
         @user = User.from_omniauth(request.env["omniauth.auth"])
-        session[:user_id] = @user.id
-        sign_in_and_redirect @user
+        # session[:user_id] = @user.id
+        sign_in @user
+        render :'welcome/update_email'
     end
 end
