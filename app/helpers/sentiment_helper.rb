bb5274032517513b0dbf0f1caa3c0ca0a3c25580
catkhuu <sunshine.dumbo@gmail.com>
2017-01-10 09:23:58 -0500

    Extract Microsoft Cognitive Services API call into helper. Further refactor get_emotions method.

---
+++ app/helpers/sentiment_helper.rb
@@ -0,0 +1,37 @@
+module SentimentHelper
+  def analyze(text)
+    uri = URI('https://westus.api.cognitive.microsoft.com/text/analytics/v2.0/sentiment')
+
+    request = Net::HTTP::Post.new(uri.request_uri)
+    # Request headers
+    request['Content-Type'] = 'application/json'
+    # Request headers
+    request['Ocp-Apim-Subscription-Key'] = ENV["MICROSOFT_KEY"]
+    # Request body
+    request.body = {
+      "documents": [
+        {
+          "language": "en",
+          "id": "string",
+          "text": text
+        }
+      ]
+    }.to_json
+
+    response = Net::HTTP.start(uri.host, uri.port, :use_ssl => uri.scheme == 'https') do |http|
+        http.request(request)
+    end
+  end
+
+  def get_emotions(analysis)
+    score = JSON.parse(analysis.body)["documents"][0]["score"]
+    if score < 0.33
+      'sad'
+    elsif score < 0.66
+      'ok'
+    else
+      'happy'
+    end
+  end
+
+end
